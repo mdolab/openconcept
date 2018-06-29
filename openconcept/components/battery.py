286f7af33205f727f33f8deeb736a6490f4f9689
Ben Brelje <bbrelje@umich.edu>
2018-06-29 14:20:25 -0400

    Documentation and style updates for gen, motor, battery

--- openconcept/components/battery.py
+++ openconcept/components/battery.py
@@ -1,48 +1,79 @@
+from __future__ import division
 import numpy as np
 from openmdao.api import ExplicitComponent
 from openmdao.api import Group
 
 
 class SimpleBattery(ExplicitComponent):
-    """Inputs: battery_weight, elec_load, SOC
-    Outputs: heat_out, component_cost, component_sizing_margin
-    Metadata: efficiency, specific_power, specific_energy, cost_inc, cost_base
+    """
+    A simple battery which tracks power limits and generates heat.
+    Specific energy assumption INCLUDING internal losses should be used
+    The efficiency parameter only generates heat
 
-    spec power in W/kg
-    spec energy in Wh/kg (CAREFUL! not J)
-    cost in $/kWhr
+    Input Vars
+    ----------
+    battery_weight : float
+        (scalar, kg)
+    elec_load: float
+        (n vector, W) Electric power draw upstream
+
+    Output Vars
+    -----------
+    max_energy : float
+        (scalar, Wh)
+    heat_out : float
+        (n vector, W)
+    component_cost : float
+        (scalar, USD)
+    component_sizing_margin : float
+        (n vector, dimensionless)
+
+
+    Options
+    -------
+    efficiency : float
+        (default 1.0) Shaft power efficiency. Sensible range 0.0 to 1.0
+    specific_power : float
+        (default 5000, W/kg) Rated power per unit weight
+    specific_energy : float
+        (default 300, !!!! Wh/kg) Battery energy per unit weight NOTE UNITS
+    cost_inc : float
+        (default 50, USD/kg) Cost per unit weight
+    cost_base : float
+        (default 1 USD) Base cost
     """
     def initialize(self):
         self.options.declare('num_nodes', default=1, desc='Number of flight/control conditions')
-        #define technology factors
         self.options.declare('efficiency', default=1., desc='Efficiency (dimensionless)')
-        self.options.declare('specific_power', default=5000., desc='Battery specific power (W/kg)' )
-        self.options.declare('specific_energy', default=300., desc='Battery spec energy CAREFUL: (Wh/kg)')
+        self.options.declare('specific_power', default=5000., desc='Battery specific power (W/kg)')
+        self.options.declare('specific_energy', default=300., desc='Battery spec energy')
         self.options.declare('cost_inc', default=50., desc='$ cost per kg')
-        self.options.declare('cost_base', default=1., desc= '$ cost base')
+        self.options.declare('cost_base', default=1., desc='$ cost base')
 
     def setup(self):
         nn = self.options['num_nodes']
         self.add_input('battery_weight', units='kg', desc='Total battery pack weight')
-        self.add_input('elec_load', units='W', desc='Electrical load drawn',shape=(nn,))
+        self.add_input('elec_load', units='W', desc='Electrical load drawn', shape=(nn,))
 
-        #outputs and partials
         eta_b = self.options['efficiency']
         e_b = self.options['specific_energy']
         p_b = self.options['specific_power']
         cost_inc = self.options['cost_inc']
         cost_base = self.options['cost_base']
 
-        self.add_output('heat_out', units='W', desc='Waste heat out',shape=(nn,))
+        self.add_output('heat_out', units='W', desc='Waste heat out', shape=(nn,))
         self.add_output('component_cost', units='USD', desc='Battery cost')
-        self.add_output('component_sizing_margin', desc='Load fraction of capable power',shape=(nn,))
-        self.add_output('max_energy',units='W*h')
+        self.add_output('component_sizing_margin',
+                        desc='Load fraction of capable power', shape=(nn,))
+        self.add_output('max_energy', units='W*h')
 
-        self.declare_partials('heat_out', 'elec_load', val=(1-eta_b)*np.ones(nn),rows=range(nn),cols=range(nn))
-        self.declare_partials('component_cost','battery_weight', val=cost_inc)
-        self.declare_partials('component_sizing_margin','battery_weight')
-        self.declare_partials('component_sizing_margin','elec_load',rows=range(nn),cols=range(nn))
-        self.declare_partials('max_energy','battery_weight',val=e_b)
+        self.declare_partials('heat_out', 'elec_load', val=(1 - eta_b) * np.ones(nn),
+                              rows=range(nn), cols=range(nn))
+        self.declare_partials('component_cost', 'battery_weight', val=cost_inc)
+        self.declare_partials('component_sizing_margin', 'battery_weight')
+        self.declare_partials('component_sizing_margin', 'elec_load',
+                              rows=range(nn), cols=range(nn))
+        self.declare_partials('max_energy', 'battery_weight', val=e_b)
 
     def compute(self, inputs, outputs):
         eta_b = self.options['efficiency']
@@ -59,5 +90,6 @@ class SimpleBattery(ExplicitComponent):
     def compute_partials(self, inputs, J):
         eta_b = self.options['efficiency']
         p_b = self.options['specific_power']
-        J['component_sizing_margin','elec_load'] = 1 / (p_b * inputs['battery_weight'])
-        J['component_sizing_margin','battery_weight'] = - inputs['elec_load'] / (p_b * inputs['battery_weight'] ** 2)
+        J['component_sizing_margin', 'elec_load'] = 1 / (p_b * inputs['battery_weight'])
+        J['component_sizing_margin', 'battery_weight'] = - (inputs['elec_load'] /
+                                                            (p_b * inputs['battery_weight'] ** 2))
--- openconcept/components/generator.py
+++ openconcept/components/generator.py
@@ -1,54 +1,85 @@
+from __future__ import division
 import numpy as np
 from openmdao.api import ExplicitComponent
 from openmdao.api import Group
 
 
 class SimpleGenerator(ExplicitComponent):
+    """
+    A simple generator which transforms shaft power into electrical power
 
-    """Inputs: shaft_power_in, elec_power_rating
-    Outputs: elec_power_out, heat_out, component_cost, component_weight, component_sizing_margin
-    Metadata: efficiency, weight_inc, weight_base, cost_inc, cost_base
+    Input Vars
+    ----------
+    shaft_power_in : float
+        (n vector, W)
+    elec_power_rating: float
+        (scalar, W) Electric (not mech) design power
 
-    Weights in kg/w, cost in $/W
+    Output Vars
+    -----------
+    elec_power_out : float
+        (n vector, W)
+    heat_out : float
+        (n vector, W)
+    component_cost : float
+        (scalar, USD)
+    component_weight : float
+        (scalar, kg)
+    component_sizing_margin : float
+        (n vector, dimensionless)
+
+
+    Options
+    -------
+    efficiency : float
+        (default 1.0) Shaft power efficiency. Sensible range 0.0 to 1.0
+    weight_inc : float
+        (default 1/5000, kg/W) Weight per unit rated power
+    weight_base : float
+        (default 0, kg) Base weight
+    cost_inc : float
+        (default 0.134228, USD/W) Cost per unit rated power
+    cost_base : float
+        (default 1 USD) Base cost
     """
     def initialize(self):
         self.options.declare('num_nodes', default=1, desc='Number of flight/control conditions')
 
-        #define technology factors
+        # define technology factors
         self.options.declare('efficiency', default=1., desc='Efficiency (dimensionless)')
-        self.options.declare('weight_inc', default=1./5000, desc='kg/W')
+        self.options.declare('weight_inc', default=1 / 5000, desc='kg/W')
         self.options.declare('weight_base', default=0., desc='kg base weight')
-        self.options.declare('cost_inc', default=100.0/745.0, desc='$ cost per watt')
-        self.options.declare('cost_base', default=1., desc= '$ cost base')
+        self.options.declare('cost_inc', default=100.0 / 745.0, desc='$ cost per watt')
+        self.options.declare('cost_base', default=1., desc='$ cost base')
 
     def setup(self):
         nn = self.options['num_nodes']
-        self.add_input('shaft_power_in', units='W', desc='Input shaft power',shape=(nn,))
+        self.add_input('shaft_power_in', units='W', desc='Input shaft power', shape=(nn,))
         self.add_input('elec_power_rating', units='W', desc='Rated output power')
 
-        #outputs and partials
+        # outputs and partials
         eta_g = self.options['efficiency']
         weight_inc = self.options['weight_inc']
         weight_base = self.options['weight_base']
         cost_inc = self.options['cost_inc']
         cost_base = self.options['cost_base']
 
-        self.add_output('elec_power_out', units='W', desc='Output electric power',shape=(nn,))
-        self.add_output('heat_out', units='W', desc='Waste heat out',shape=(nn,))
+        self.add_output('elec_power_out', units='W', desc='Output electric power', shape=(nn,))
+        self.add_output('heat_out', units='W', desc='Waste heat out', shape=(nn,))
         self.add_output('component_cost', units='USD', desc='Generator component cost')
         self.add_output('component_weight', units='kg', desc='Generator component weight')
-        self.add_output('component_sizing_margin', desc='Fraction of rated power',shape=(nn,))
+        self.add_output('component_sizing_margin', desc='Fraction of rated power', shape=(nn,))
 
-        self.declare_partials('elec_power_out','shaft_power_in', val=eta_g*np.ones(nn),rows=range(nn),cols=range(nn))
-        self.declare_partials('heat_out', 'shaft_power_in', val=(1-eta_g)*np.ones(nn),rows=range(nn),cols=range(nn))
-        self.declare_partials('component_cost','elec_power_rating', val=cost_inc)
-        self.declare_partials('component_weight','elec_power_rating', val=weight_inc)
-        self.declare_partials('component_sizing_margin','shaft_power_in',rows=range(nn),cols=range(nn))
-        self.declare_partials('component_sizing_margin','elec_power_rating')
+        self.declare_partials('elec_power_out', 'shaft_power_in',
+                              val=eta_g * np.ones(nn), rows=range(nn), cols=range(nn))
+        self.declare_partials('heat_out', 'shaft_power_in',
+                              val=(1 - eta_g) * np.ones(nn), rows=range(nn), cols=range(nn))
+        self.declare_partials('component_cost', 'elec_power_rating', val=cost_inc)
+        self.declare_partials('component_weight', 'elec_power_rating', val=weight_inc)
+        self.declare_partials('component_sizing_margin', 'shaft_power_in',
+                              rows=range(nn), cols=range(nn))
+        self.declare_partials('component_sizing_margin', 'elec_power_rating')
 
-
-
-            
     def compute(self, inputs, outputs):
         eta_g = self.options['efficiency']
         weight_inc = self.options['weight_inc']
@@ -60,26 +91,11 @@ class SimpleGenerator(ExplicitComponent):
         outputs['heat_out'] = inputs['shaft_power_in'] * (1 - eta_g)
         outputs['component_cost'] = inputs['elec_power_rating'] * cost_inc + cost_base
         outputs['component_weight'] = inputs['elec_power_rating'] * weight_inc + weight_base
-        outputs['component_sizing_margin'] = inputs['shaft_power_in'] * eta_g / inputs['elec_power_rating']
-        
+        outputs['component_sizing_margin'] = (inputs['shaft_power_in'] *
+                                              eta_g / inputs['elec_power_rating'])
+
     def compute_partials(self, inputs, J):
         eta_g = self.options['efficiency']
-        J['component_sizing_margin','shaft_power_in'] = eta_g / inputs['elec_power_rating']
-        J['component_sizing_margin','elec_power_rating'] = - eta_g * inputs['shaft_power_in'] / inputs['elec_power_rating'] ** 2
-
-
-if __name__ == "__main__":
-    from openmdao.api import IndepVarComp, Problem
-    prob = Problem()
-    prob.model = Group()
-    prob.model.add_subsystem('P_in',IndepVarComp('P_i',val=100.,units='kW'))
-    prob.model.add_subsystem('P_rated',IndepVarComp('P_r',val=150.,units='kW'))
-    prob.model.add_subsystem('gen',SimpleGenerator(efficiency=0.98,weight_inc=0.2,weight_base=20,cost_inc=0.05,cost_base=10000.))
-    prob.model.connect('P_in.P_i','gen.shaft_power_in')
-    prob.model.connect('P_rated.P_r','gen.elec_power_rating')
-    prob.setup()
-    prob.run_model()
-    print(prob['gen.component_cost'])
-    print(prob['gen.elec_power_out'])
-    data = prob.check_partials()
-
+        J['component_sizing_margin', 'shaft_power_in'] = eta_g / inputs['elec_power_rating']
+        J['component_sizing_margin', 'elec_power_rating'] = - (eta_g * inputs['shaft_power_in'] /
+                                                               inputs['elec_power_rating'] ** 2)
