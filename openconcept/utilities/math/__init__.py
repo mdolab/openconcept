1441b3b24ee946cc54da02a613a226d10a301ec5
Ben Brelje <benjamin.brelje@gmail.com>
2020-07-26 21:43:12 -0400

    Deprecate unused functions and tests

--- openconcept/utilities/math/__init__.py
+++ openconcept/utilities/math/__init__.py
@@ -1,5 +1,4 @@
 from .add_subtract_comp import AddSubtractComp
 from .combine_split_comp import VectorConcatenateComp, VectorSplitComp
 from .multiply_divide_comp import ElementMultiplyDivideComp
-from .simpson_integration import IntegrateQuantity
 from .sum_comp import SumComp
--- openconcept/utilities/math/tests/test_math.py
+++ openconcept/utilities/math/tests/test_math.py
@@ -4,78 +4,7 @@ import pytest
 import numpy as np
 from openmdao.utils.assert_utils import assert_near_equal, assert_check_partials
 from openmdao.api import IndepVarComp, Group, Problem
-from openconcept.utilities.math.simpson_integration import IntegrateQuantity
 from openconcept.utilities.math.derivatives import FirstDerivative
-
-class SimpsonTestGroup(Group):
-    """An OpenMDAO group to test the simpson integration capability
-    """
-    def initialize(self):
-        self.options.declare('n_simp_intervals',default=1,desc="Number of Simpson intevals to use")
-    def setup(self):
-        ni = self.options['n_simp_intervals']
-        iv = self.add_subsystem('iv', IndepVarComp())
-        self.add_subsystem('integrate', IntegrateQuantity(num_intervals=ni,diff_units='s',quantity_units='m'),promotes_outputs=['*'])
-        iv.add_output('start_pt', val=0, units='s')
-        iv.add_output('end_pt', val=1, units='s')
-        iv.add_output('function', val=np.ones(2*ni+1), units='m/s')
-        self.connect('iv.start_pt','integrate.lower_limit')
-        self.connect('iv.end_pt','integrate.upper_limit')
-        self.connect('iv.function','integrate.rate')
-        #output is 'delta quantity'
-
-class SimpsonTestCase(unittest.TestCase):
-    def test_uniform_single(self):
-        prob = Problem(SimpsonTestGroup(n_simp_intervals=1))
-        prob.setup(check=True)
-        prob.run_model()
-        assert_near_equal(prob['delta_quantity'],1.,tolerance=1e-15)
-        partials = prob.check_partials(method='fd',compact_print=True)
-        assert_check_partials(partials)
-
-    def test_uniform(self):
-        prob = Problem(SimpsonTestGroup(n_simp_intervals=5))
-        prob.setup(check=True)
-        prob.run_model()
-        assert_near_equal(prob['delta_quantity'],1.,tolerance=1e-15)
-        partials = prob.check_partials(method='fd',compact_print=True)
-        assert_check_partials(partials)
-
-    def test_endpoints(self):
-        prob = Problem(SimpsonTestGroup(n_simp_intervals=5))
-        prob.setup(check=False)
-        prob['iv.end_pt'] = 2
-        prob.run_model()
-        assert_near_equal(prob['delta_quantity'],2.,tolerance=1e-15)
-
-    def test_function_level(self):
-        prob = Problem(SimpsonTestGroup(n_simp_intervals=5))
-        prob.setup(check=False)
-        prob['iv.function'] = 3*np.ones(2*5+1)
-        prob.run_model()
-        assert_near_equal(prob['delta_quantity'],3.,tolerance=1e-15)
-
-    def test_trig_function_approx(self):
-        prob = Problem(SimpsonTestGroup(n_simp_intervals=5))
-        prob.setup(check=False)
-        x = np.linspace(0,np.pi,2*5+1)
-        prob['iv.end_pt'] = np.pi
-        prob['iv.function'] = np.sin(x)
-        prob.run_model()
-        assert_near_equal(prob['delta_quantity'],2.,tolerance=1e-4)
-        partials = prob.check_partials(method='fd',compact_print=True)
-        assert_check_partials(partials)
-
-    def test_cubic_polynomial_exact(self):
-        prob = Problem(SimpsonTestGroup(n_simp_intervals=5))
-        prob.setup(check=False)
-        x = np.linspace(0,1,2*5+1)
-        prob['iv.function'] = x**3-2*x**2+2.5*x-4
-        prob.run_model()
-        assert_near_equal(prob['delta_quantity'],-3.1666666666666666666666666666666666666,tolerance=1e-16)
-        partials = prob.check_partials(method='fd',compact_print=True)
-        assert_check_partials(partials)
-
 
 class FirstDerivativeTestGroup(Group):
     """An OpenMDAO group to test the differentiation tools
