b82dd5de091d3d6e97c0c28fda42a7431f1c88b4
eytanadler <eytana@umich.edu>
2021-10-01 15:49:36 -0400

    Getting there on the OpenAeroStruct aerodynamic integration...

--- openconcept/analysis/openaerostruct/drag_polar.py
+++ openconcept/analysis/openaerostruct/drag_polar.py
@@ -2,8 +2,17 @@ from __future__ import division
 
 import numpy as np
 import openmdao.api as om
+from time import time
+
+# OpenAeroStruct
 from openaerostruct.geometry.geometry_mesh_transformations import Rotate
 from openaerostruct.aerodynamics.aero_groups import AeroPoint
+
+# Atmospheric calculations
+from openconcept.analysis.atmospherics.temperature_comp import TemperatureComp
+from openconcept.analysis.atmospherics.pressure_comp import PressureComp
+from openconcept.analysis.atmospherics.density_comp import DensityComp
+from openconcept.analysis.atmospherics.speedofsound_comp import SpeedOfSoundComp
 
 class OASDataGen(om.ExplicitComponent):
     """
@@ -25,6 +34,8 @@ class OASDataGen(om.ExplicitComponent):
     ac|geom|twist : float
         List of twist angles at control points of spline (vector, degrees)
         NOTE: length of vector is num_twist (set in options)
+    fltcond|TempIncrement : float
+        Temperature increment for non-standard day (scalar, degC)
 
     Outputs
     -------
@@ -65,6 +76,8 @@ class OASDataGen(om.ExplicitComponent):
         self.add_input('ac|geom|AR')
         self.add_input('ac|geom|taper')
         self.add_input('ac|geom|c4sweep', units='deg')
+        self.add_input('ac|geom|twist', shape=(self.options["num_twist"],), units='deg')
+        self.add_input('fltcond|TempIncrement', val=0., units='degC')
 
         n_alpha = self.options['alpha'].size
         n_Mach = self.options['Mach'].size
@@ -79,12 +92,121 @@ class OASDataGen(om.ExplicitComponent):
         self.AR = -1
         self.taper = -1
         self.c4sweep = -1
+        self.twist = -1*np.ones((self.options["num_twist"],))
         self.alpha, self.Mach, self.alt = np.meshgrid(self.options['alpha_train'],
                                                       self.options['Mach_train'],
                                                       self.options['alt_train'],
                                                       indexing='ij')
         self.CL = np.zeros((n_alpha, n_Mach, n_alt))
         self.CD = np.zeros((n_alpha, n_Mach, n_alt))
+    
+    def compute(self, inputs, outputs):
+        S = inputs["ac|geom|S_ref"]
+        AR = inputs["ac|geom|AR"]
+        taper = inputs["ac|geom|taper"]
+        sweep = inputs["ac|geom|c4sweep"]
+        twist = inputs["ac|geom|twist"]
+
+
+"""
+Generates training data and its total derivatives by
+calling OpenAeroStruct at each training point.
+
+Inputs
+------
+inputs : dict
+    A dictionary containing the following entries:
+    alpha_grid : ndarray
+        Angle of attack (3D meshgrid 'ij'-style ndarray, degrees)
+    Mach_number_grid : mdarray
+        Mach number (3D meshgrid 'ij'-style ndarray, dimensionless)
+    alt_grid : ndarray
+        Altitude (3D meshgrid 'ij'-style ndarray, m)
+    TempIncrement : float
+        Temperature increment for non-standard day (scalar, degC)
+    S_ref : float
+        Wing planform area (scalar, m^2)
+    AR : float
+        Wing aspect ratio (scalar, dimensionless)
+    taper : float
+        Wing taper ratio (scalar, dimensionless)
+    c4sweep : float
+        Wing sweep measured at quarter chord (scalar, degrees)
+    twist : float
+        List of twist angles at control points of spline (vector, degrees)
+        NOTE: length of vector is num_twist (set in options of OASDataGen)
+    num_x: int
+        number of points in x (streamwise) direction (scalar, dimensionless)
+    num_y: int
+        number of points in y (spanwise) direction for one wing because
+        uses symmetry (scalar, dimensionless)
+TODO: add optional surf_dict input? Add any addition options to the default surf_dict in the VLM group
+
+Outputs
+-------
+data : dict
+    A dictionary containing the following entries:
+    CL : ndarray
+        Lift coefficients at training points (3D meshgrid 'ij'-style ndarray, dimensionless)
+    CD : ndarray
+        Drag coefficients at training points (3D meshgrid 'ij'-style ndarray, dimensionless)
+    TODO: derivatives?
+
+"""
+def compute_training_data(inputs, surf_dict=None):
+    # Initialize output arrays
+    CL = np.zeros(inputs['alpha_grid'].shape)
+    CD = np.zeros(inputs['alpha_grid'].shape)
+
+    # Set up OpenAeroStruct problem
+    p = om.Problem()
+    p.model.add_subsystem('aero_analysis', VLM(num_x=inputs['num_x'],
+                                               num_y=inputs['num_y'],
+                                               num_twist=inputs['twist'].size),
+                           promotes=['*'])
+
+    # Set design variables
+    p.model.set_input_defaults('fltcond|TempIncrement', val=inputs['TempIncrement'], units='degC')
+    p.model.set_input_defaults('ac|geom|S_ref', val=inputs['S_ref'], units='m**2')
+    p.model.set_input_defaults('ac|geom|AR', val=inputs['AR'])
+    p.model.set_input_defaults('ac|geom|taper', val=inputs['taper'])
+    p.model.set_input_defaults('ac|geom|c4sweep', val=inputs['c4sweep'], units='deg')
+    p.model.set_input_defaults('ac|geom|twist', val=inputs['twist'], units='deg')
+
+    p.setup()
+
+    # Iterate through the training points and evaluate the model
+    iter = 0
+    total = inputs['alpha_grid'].shape[0]*inputs['alpha_grid'].shape[1]*inputs['alpha_grid'].shape[2]
+    model_time = 0
+    deriv_time = 0
+    for i in range(inputs['alpha_grid'].shape[0]):
+        for j in range(inputs['alpha_grid'].shape[1]):
+            for k in range(inputs['alpha_grid'].shape[2]):
+                # Set the values for the current training point in the model
+                p.set_val('fltcond|alpha', inputs['alpha_grid'][i,j,k], units='deg')
+                p.set_val('fltcond|M', inputs['Mach_number_grid'][i,j,k])
+                p.set_val('fltcond|h', inputs['alt_grid'][i,j,k], units='m')
+
+                # Run the models and pull the lift and drag values out
+                print(f"Generating training data...{iter/total*100:.1f}%", end='\r')
+                iter += 1
+                start = time()
+                p.run_model()
+                model_time += time() - start
+                CL[i,j,k] = p.get_val('fltcond|CL')
+                CD[i,j,k] = p.get_val('fltcond|CD')
+
+                start = time()
+                deriv = p.compute_totals(of=['fltcond|CL', 'fltcond|CD'], wrt=['ac|geom|S_ref', 'ac|geom|AR',
+                                         'ac|geom|taper', 'ac|geom|c4sweep', 'ac|geom|twist', 'fltcond|TempIncrement'])
+                deriv_time += time() - start
+    
+    print(f"Model convergence time = {model_time} sec")
+    print(f"Derivative computation time = {deriv_time} sec")
+    data = {'CL': CL, 'CD': CD}
+    return data
+
 
 class VLM(om.Group):
     """
@@ -96,14 +218,10 @@ class VLM(om.Group):
         Angle of attack (scalar, degrees)
     fltcond|M : float
         Mach number (vector, dimensionless)
-    fltcond|TempIncrement : float
-        Temperature increment for non-standard day (scalar, degC)
-    # fltcond|rho : float
-    #     Density (vector, kg/m^3)
-    # fltcond|Utrue : float
-    #     True airspeed (vector, m/s)
     fltcond|h : float
         Altitude (vector, m)
+    fltcond|TempIncrement : float
+        Temperature increment for non-standard day (scalar, degC)
     ac|geom|S_ref : float
         Wing planform area (scalar, m^2)
     ac|geom|AR : float
@@ -143,7 +261,9 @@ class VLM(om.Group):
         ny = int(self.options["num_y"])
         n_twist = int(self.options["num_twist"])
 
-        # Set up mesh
+        # =================================================================
+        #                            Set up mesh
+        # =================================================================
         self.add_subsystem("mesh", PlanformMesh(num_x=nx, num_y=ny),
                            promotes_inputs=[("S", "ac|geom|S_ref"), ("AR", "ac|geom|AR"),
                                             ("taper", "ac|geom|taper"), ("sweep", "ac|geom|c4sweep")])
@@ -164,15 +284,38 @@ class VLM(om.Group):
         self.connect("twist_bsp.twist", "twist_mesh.twist")
         self.connect("mesh.mesh", "twist_mesh.in_mesh")
 
+        # =================================================================
+        #              Compute atmospheric and fluid properties
+        # =================================================================
+        self.add_subsystem('temp', TemperatureComp(num_nodes=1),
+                           promotes_inputs=['fltcond|h', 'fltcond|TempIncrement'])
+        self.add_subsystem('pressure', PressureComp(num_nodes=1),
+                           promotes_inputs=['fltcond|h'])
+        self.add_subsystem('density', DensityComp(num_nodes=1))
+        self.connect('temp.fltcond|T', 'density.fltcond|T')
+        self.connect('pressure.fltcond|p', 'density.fltcond|p')
+        self.add_subsystem('sound_speed', SpeedOfSoundComp(num_nodes=1))
+        self.connect('temp.fltcond|T', 'sound_speed.fltcond|T')
+        self.add_subsystem('airspeed', om.ExecComp("Utrue = Mach * a",
+                                                   Utrue={'units': 'm/s', 'val': 200.},
+                                                   a={'units': 'm/s', 'val': 300.}),
+                           promotes_inputs=[('Mach', 'fltcond|M')])
+        self.connect('sound_speed.fltcond|a', 'airspeed.a')
+
         # Compute dimensionalized Reynolds number (use linear interpolation from standard atmosphere up
         # to 35k ft to estimate dynamic viscosity)
         self.add_subsystem("Re_calc", om.ExecComp("re = rho * u / (-3.329134*10**(-10) * h + 1.792398*10**(-5))",
-                                                  re={"units": "1/m", "val": 1e6, "ref": 1e6},
+                                                  re={"units": "1/m", "val": 1e6},
                                                   rho={"units": "kg/m**3", "val": 1.},
                                                   u={"units": "m/s", "val": 100.},
                                                   h={"units": "m", "val": 1.}),
-                           promotes_inputs=[("rho", "fltcond|rho"), ("u", "fltcond|Utrue"), ("h", "fltcond|h")])
+                           promotes_inputs=[("h", "fltcond|h")])
+        self.connect('density.fltcond|rho', 'Re_calc.rho')
+        self.connect('airspeed.Utrue', 'Re_calc.u')
 
+        # =================================================================
+        #                       Call OpenAeroStruct
+        # =================================================================
         surf_dict = {
             "name": "wing",
             "mesh": np.zeros((nx, ny, 3)),  # this must be defined
@@ -203,17 +346,18 @@ class VLM(om.Group):
             }
 
         self.add_subsystem("aero_point", AeroPoint(surfaces=[surf_dict]),
-                           promotes_inputs=[("v", "fltcond|Utrue"), ("Mach_number", "fltcond|M"),
-                                            ("rho", "fltcond|rho"), ("alpha", "fltcond|alpha")],
+                           promotes_inputs=[("Mach_number", "fltcond|M"), ("alpha", "fltcond|alpha")],
                            promotes_outputs=[(f"{surf_dict['name']}_perf.CD", "fltcond|CD"),
                                              (f"{surf_dict['name']}_perf.CL", "fltcond|CL")])
         self.connect("twist_mesh.mesh", [f"aero_point.{surf_dict['name']}.def_mesh",
                                          f"aero_point.aero_states.{surf_dict['name']}_def_mesh"])
+        self.connect('airspeed.Utrue', 'aero_point.v')
+        self.connect('density.fltcond|rho', 'aero_point.rho')
         self.connect("Re_calc.re", "aero_point.re")
 
-        # Set default input values of values that come from multiple sources
-        self.set_input_defaults("fltcond|rho", val=1., units="kg/m**3")
-        self.set_input_defaults("fltcond|Utrue", val=200., units="kn")
+        # Set input defaults for inputs that go to multiple locations
+        self.set_input_defaults('fltcond|M', 0.1)
+        self.set_input_defaults('fltcond|alpha', 0.)
 
         # Set the thickness to chord ratio for wave and viscous drag calculation.
         # It must have a thickness to chord ratio for each panel, so there must be
@@ -229,35 +373,35 @@ class VLM(om.Group):
                              f"with either one or ny-1 elements, not {surf_dict['t_over_c']}")
 
 
-"""
-Generate an OpenAeroStruct mesh based on basic wing design parameters.
-Resulting mesh is for a half wing (meant to use with OpenAeroStruct symmetry),
-but the input reference area is for the full wing.
-
-Inputs
-------
-S: float
-    full planform area (scalar, m^2)
-AR: float
-    aspect ratio (scalar, dimensionless)
-taper: float
-    taper ratio (must be >0 and <=1); tip chord / root chord (scalar, dimensionless)
-sweep: float
-    quarter chord sweep angle (scalar, degrees)
-
-Outputs
--------
-mesh: ndarray
-    OpenAeroStruct 3D mesh (num_x x num_y x 3 ndarray, m)
-
-Options
--------
-num_x: int
-    number of points in x (streamwise) direction (scalar, dimensionless)
-num_y: int
-    number of points in y (spanwise) direction (scalar, dimensionless)
-"""
 class PlanformMesh(om.ExplicitComponent):
+    """
+    Generate an OpenAeroStruct mesh based on basic wing design parameters.
+    Resulting mesh is for a half wing (meant to use with OpenAeroStruct symmetry),
+    but the input reference area is for the full wing.
+
+    Inputs
+    ------
+    S: float
+        full planform area (scalar, m^2)
+    AR: float
+        aspect ratio (scalar, dimensionless)
+    taper: float
+        taper ratio (must be >0 and <=1); tip chord / root chord (scalar, dimensionless)
+    sweep: float
+        quarter chord sweep angle (scalar, degrees)
+
+    Outputs
+    -------
+    mesh: ndarray
+        OpenAeroStruct 3D mesh (num_x x num_y x 3 ndarray, m)
+
+    Options
+    -------
+    num_x: int
+        number of points in x (streamwise) direction (scalar, dimensionless)
+    num_y: int
+        number of points in y (spanwise) direction (scalar, dimensionless)
+    """
     def initialize(self):
         self.options.declare("num_x", default=3, desc="Number of streamwise mesh points")
         self.options.declare("num_y", default=7, desc="Number of spanwise (half wing) mesh points")
@@ -309,3 +453,33 @@ class PlanformMesh(om.ExplicitComponent):
         mesh[:, :, 1] = y_mesh
 
         outputs["mesh"] = mesh
+
+
+if __name__=="__main__":
+    alpha_list = np.linspace(-10, 10, 200)
+    Mach_list = np.array([0.1])#np.linspace(0.01, 0.9, 100)
+    alt_list = np.array([.1])#0.3048*np.linspace(0, 40000, 200)
+    
+    inputs = {}
+    inputs['alpha_grid'], inputs['Mach_number_grid'], inputs['alt_grid'] = np.meshgrid(alpha_list,
+                                                                                       Mach_list,
+                                                                                       alt_list,
+                                                                                       indexing='ij')
+    inputs['TempIncrement'] = 0
+    inputs['S_ref'] = 427.8
+    inputs['AR'] = 9.82
+    inputs['taper'] = 0.149
+    inputs['c4sweep'] = 31.6
+    inputs['twist'] = np.array([-3, 1.5, 6])
+    inputs['num_x'] = 3
+    inputs['num_y'] = 5
+
+    res = compute_training_data(inputs)
+
+    import matplotlib.pyplot as plt
+    plt.plot(res['CD'][:,0,0], res['CL'][:,0,0])
+    plt.xlabel('CD')
+    plt.ylabel('CL')
+    plt.show()
+
+    
