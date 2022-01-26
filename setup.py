5d46f387f6f3951c7587dd9d9e851e6740cfa891
eytanadler <eytana@umich.edu>
2022-01-26 12:23:30 -0500

    Moved sphinx_mdolab_theme from .[docs] to .[testing]

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
         'openmdao>=3.10.0',
     ],
     extras_require = {
-        'testing':  ["pytest", "openmdao[docs]", "redbaron"],
-        'docs': ["sphinx_mdolab_theme"]
+        'testing':  ["pytest", "openmdao[docs]", "redbaron", "sphinx_mdolab_theme"]
       },
 )
