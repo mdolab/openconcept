027ac618d1fb60f75acbba2341f3e360ac1f8a4f
eytanadler <eytana@umich.edu>
2022-02-04 13:22:58 -0500

    Removed commented out code in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('./_exts'))
 # so we add it to the path
 this_directory = os.path.abspath(os.path.dirname(__file__))
 
-sys.path.insert(0, os.path.abspath(openconcept.__path__[0]+r'/docs/_exts'))
+# sys.path.insert(0, os.path.abspath(openconcept.__path__[0]+r'/docs/_exts'))
 
 
 def generate_src_docs(dir, top, packages):
@@ -239,31 +239,10 @@ language = None
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'sphinx_rtd_theme'
-# html_style = 'css/override_theme.css'
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
