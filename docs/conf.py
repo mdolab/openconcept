8943736658d350de0afecf2fa4c76d3d622e2b85
eytanadler <eytana@umich.edu>
2022-06-08 14:00:10 -0400

    Debugging sphinx build failure

--- docs/conf.py
+++ docs/conf.py
@@ -234,7 +234,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 
 # -- Options for HTML output -------------------------------------------------
