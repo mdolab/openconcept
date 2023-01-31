261b7a953aa51174bd18054579ec661c73f7d26f
Eytan Adler <eytana@umich.edu>
2023-01-31 14:45:40 -0500

    Ran black

--- openconcept/atmospherics/viscosity_comp.py
+++ openconcept/atmospherics/viscosity_comp.py
@@ -57,7 +57,11 @@ class ViscosityComp(ExplicitComponent):
         low_mid_smooth = 0.5 * np.tanh(0.01 * (h_m - 11.1e3)) + 0.5
         mid_high_smooth = 0.5 * np.tanh(0.01 * (h_m - 20e3)) + 0.5
 
-        outputs["fltcond|mu"] = mu_lower_alt * (1 - low_mid_smooth) + mu_mid_alt * low_mid_smooth * (1 - mid_high_smooth) + mu_higher_alt * mid_high_smooth
+        outputs["fltcond|mu"] = (
+            mu_lower_alt * (1 - low_mid_smooth)
+            + mu_mid_alt * low_mid_smooth * (1 - mid_high_smooth)
+            + mu_higher_alt * mid_high_smooth
+        )
         outputs["fltcond|visc_kin"] = outputs["fltcond|mu"] / inputs["fltcond|rho"]
 
     def compute_partials(self, inputs, partials):
@@ -72,7 +76,11 @@ class ViscosityComp(ExplicitComponent):
         low_mid_smooth = 0.5 * np.tanh(0.01 * (h_m - 11.1e3)) + 0.5
         mid_high_smooth = 0.5 * np.tanh(0.01 * (h_m - 20e3)) + 0.5
 
-        mu = mu_lower_alt * (1 - low_mid_smooth) + mu_mid_alt * low_mid_smooth * (1 - mid_high_smooth) + mu_higher_alt * mid_high_smooth
+        mu = (
+            mu_lower_alt * (1 - low_mid_smooth)
+            + mu_mid_alt * low_mid_smooth * (1 - mid_high_smooth)
+            + mu_higher_alt * mid_high_smooth
+        )
 
         # Partials
         dmu_lower_alt_dh = -3.413179e-10
@@ -82,9 +90,13 @@ class ViscosityComp(ExplicitComponent):
         dmid_high_smooth_dh = 0.5 * (1 - np.tanh(0.01 * (h_m - 20e3)) ** 2) * 0.01
 
         partials["fltcond|mu", "fltcond|h"] = (
-            dmu_lower_alt_dh * (1 - low_mid_smooth) - mu_lower_alt * dlow_mid_smooth_dh
-            + dmu_mid_alt_dh * low_mid_smooth * (1 - mid_high_smooth) + mu_mid_alt * dlow_mid_smooth_dh * (1 - mid_high_smooth) - mu_mid_alt * low_mid_smooth * dmid_high_smooth_dh
-            + dmu_higher_alt_dh * mid_high_smooth + mu_higher_alt * dmid_high_smooth_dh
+            dmu_lower_alt_dh * (1 - low_mid_smooth)
+            - mu_lower_alt * dlow_mid_smooth_dh
+            + dmu_mid_alt_dh * low_mid_smooth * (1 - mid_high_smooth)
+            + mu_mid_alt * dlow_mid_smooth_dh * (1 - mid_high_smooth)
+            - mu_mid_alt * low_mid_smooth * dmid_high_smooth_dh
+            + dmu_higher_alt_dh * mid_high_smooth
+            + mu_higher_alt * dmid_high_smooth_dh
         )
         partials["fltcond|visc_kin", "fltcond|h"] = partials["fltcond|mu", "fltcond|h"] / inputs["fltcond|rho"]
-        partials["fltcond|visc_kin", "fltcond|rho"] = -mu / inputs["fltcond|rho"]**2
+        partials["fltcond|visc_kin", "fltcond|rho"] = -mu / inputs["fltcond|rho"] ** 2
