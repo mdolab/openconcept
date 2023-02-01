51d509102945391e0c171edf4807feace0865010
Eytan Adler <eytana@umich.edu>
2023-02-01 18:34:21 -0500

    Starting new thermal model

--- openconcept/utilities/constants.py
+++ openconcept/utilities/constants.py
@@ -1,3 +1,4 @@
 GRAV_CONST = 9.80665  # m/s^2, gravitational constant
 UNIVERSAL_GAS_CONST = 8.3145  # J/(mol-K), universal gas constant
 MOLEC_WEIGHT_H2 = 2.01588e-3  # kg/mol, molecular weight of hydrogen
+STEFAN_BOLTZMANN_CONST = 5.670374419e-8  # W/(m^2 K^4), Stefan-Boltzmann constant
