d32fa3031191aa619540d8efbaadfaaded437269
Eytan Adler <eytana@umich.edu>
2023-02-08 11:26:58 -0500

    Added rubberized turbofan that can do hydrogen

--- openconcept/propulsion/__init__.py
+++ openconcept/propulsion/__init__.py
@@ -4,6 +4,7 @@ from .generator import SimpleGenerator
 from .motor import SimpleMotor
 from .N3 import N3, N3Hybrid
 from .propeller import SimplePropeller, WeightCalc, ThrustCalc, PropCoefficients
+from .rubberized_turbofan import RubberizedTurbofan
 from .splitter import PowerSplit
 from .turboshaft import SimpleTurboshaft
 
