726e0fa904c0170427598d086a7dfdf725923151
Eytan Adler <eytana@umich.edu>
2023-04-11 14:17:15 -0400

    Max lift coefficient estimation via the critical section method

---
+++ openconcept/aerodynamics/openaerostruct/CL_max_critical_section.py
@@ -0,0 +1,105 @@
+"""
+@File    :   CLmax_jet_transport.py
+@Date    :   2023/04/11
+@Author  :   Eytan Adler
+@Description : Max lift coefficient estimate using critical section method
+"""
+
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+import numpy as np
+import openmdao.api as om
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+from openconcept.aerodynamics.openaerostruct import VLM
+from openconcept.utilities import AddSubtractComp
+
+
+class CLmaxCriticalSectionVLM(om.Group):
+    """
+    Predict the maximum lift coefficient of the wing by solving for the
+    angle of attack where the maximum sectional lift coefficient equals the
+    provided maximum lift coefficient of the airfoil. The solution for the
+    sectional lift coefficients is done using OpenAeroStruct.
+
+    Inputs
+    ------
+    ac|aero|airfoil_Cl_max : float
+        Maximum 2D lift coefficient of the wing airfoil (scalar, dimensionless)
+    fltcond|M : float
+        Mach number for maximum CL calculation (scalar, dimensionless)
+    fltcond|h : float
+        Altitude for maximum CL calculation (scalar, m)
+    fltcond|TempIncrement : float
+        Temperature increment for maximum CL calculation (scalar, degC)
+    ac|geom|wing|OAS_mesh: float
+        OpenAeroStruct 3D mesh (num_x + 1 x num_y + 1 x 3 vector, m)
+    ac|geom|wing|toverc : float
+        Thickness to chord ratio of each streamwise strip of panels ordered from wing
+        tip to wing root; used for the viscous and wave drag calculations
+        (vector of length num_y, dimensionless)
+
+    Outputs
+    -------
+    CL_max : float
+        Maximum lift coefficient estimated using the critical section method (scalar, dimensionless)
+
+    Options
+    -------
+    num_x : int
+        Number of panels in x (streamwise) direction (scalar, dimensionless)
+    num_y : int
+        Number of panels in y (spanwise) direction for one wing because
+        uses symmetry (scalar, dimensionless)
+    surf_options : dict
+        Dictionary of OpenAeroStruct surface options; any options provided here
+        will override the default ones; see the OpenAeroStruct documentation for more information.
+        Because the geometry transformations are excluded in this model (to simplify the interface),
+        the <transformation>_cp options are not supported. The t_over_c option is also removed since
+        it is provided instead as an input.
+    rho : float or int
+        Constraint aggregation factor for sectional lift coefficient aggregation, by default 200
+    """
+
+    def initialize(self):
+        self.options.declare("num_x", default=2, desc="Number of streamwise mesh panels")
+        self.options.declare("num_y", default=6, desc="Number of spanwise (half wing) mesh panels")
+        self.options.declare("surf_options", default=None, desc="Dictionary of OpenAeroStruct surface options")
+        self.options.declare("rho", default=200, types=(float, int), desc="Sectional CL aggregation factor")
+
+    def setup(self):
+        # -------------- Simulate the wing in OpenAeroStruct --------------
+        self.add_subsystem(
+            "VLM",
+            VLM(num_x=self.options["num_x"], num_y=self.options["num_y"], surf_options=self.options["surf_options"]),
+            promotes_inputs=[
+                "fltcond|M",
+                "fltcond|h",
+                "fltcond|TempIncrement",
+                "ac|geom|wing|OAS_mesh",
+                "ac|geom|wing|toverc",
+            ],
+            promotes_outputs=[("fltcond|CL", "CL_max")],
+        )
+
+        # -------------- Aggregate the sectional lift coefficients to find the max --------------
+        self.add_subsystem("max_sectional_CL", om.KSComp(width=self.options["num_y"], rho=self.options["rho"]))
+        self.connect("VLM.sectional_CL", "max_sectional_CL.g")
+
+        # -------------- Solve for the angle of attack --------------
+        self.add_subsystem(
+            "sectional_CL_balance",
+            om.BalanceComp("alpha", lhs_name="sec_CL_max_VLM", rhs_name="airfoil_CL_max", val=5, units="deg"),
+            promotes_inputs=[("airfoil_CL_max", "ac|aero|airfoil_Cl_max")],
+        )
+        self.connect("max_sectional_CL.KS", "sectional_CL_balance.sec_CL_max_VLM")
+        self.connect("sectional_CL_balance.alpha", "VLM.fltcond|alpha")
+
---
+++ openconcept/aerodynamics/openaerostruct/tests/test_CL_max_critical_section.py
@@ -0,0 +1,63 @@
+"""
+@File    :   test_CL_max_critical_section.py
+@Date    :   2023/04/11
+@Author  :   Eytan Adler
+@Description : Test the critical section lift coefficient utility.
+"""
+
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+import numpy as np
+import unittest
+import openmdao.api as om
+from openmdao.utils.assert_utils import assert_near_equal
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+from openconcept.aerodynamics.openaerostruct import CLmaxCriticalSectionVLM, TrapezoidalPlanformMesh
+
+
+class CLmaxCriticalSectionVLMTestCase(unittest.TestCase):
+    def test(self):
+        nx = 2
+        ny = 5
+        p = om.Problem()
+
+        p.model.add_subsystem(
+            "mesh",
+            TrapezoidalPlanformMesh(num_x=nx, num_y=ny),
+            promotes_inputs=["*"],
+            promotes_outputs=[("mesh", "ac|geom|wing|OAS_mesh")],
+        )
+        p.model.add_subsystem("CL_max", CLmaxCriticalSectionVLM(num_x=nx, num_y=ny), promotes=["*"])
+
+        p.model.nonlinear_solver = om.NewtonSolver(solve_subsystems=True, iprint=2, maxiter=10)
+        p.model.linear_solver = om.DirectSolver()
+
+        p.setup()
+
+        p.set_val("S", 125, units="m**2")
+        p.set_val("AR", 10)
+        p.set_val("taper", 0.15)
+        p.set_val("sweep", 25, units="deg")
+
+        Cl_max_foil = 1.5
+        p.set_val("ac|aero|airfoil_Cl_max", Cl_max_foil)
+        p.set_val("fltcond|M", 0.2)
+        p.set_val("fltcond|h", 0, units="ft")
+        p.set_val("ac|geom|wing|toverc", np.full(ny, 0.12))
+
+        p.run_model()
+
+        assert_near_equal(np.max(p.get_val("VLM.sectional_CL")), Cl_max_foil, tolerance=1e-6)
+        assert_near_equal(p.get_val("max_sectional_CL.KS").item(), Cl_max_foil, tolerance=1e-13)
+
+
+if __name__ == "__main__":
+    unittest.main()
