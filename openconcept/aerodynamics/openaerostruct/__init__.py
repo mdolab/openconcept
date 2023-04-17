1b57d18a7eab1a1ad084a7e305699c063f9ad83e
Eytan Adler <eytana@umich.edu>
2023-04-17 14:26:00 -0400

    Added comps to compute wave drag

--- openconcept/aerodynamics/openaerostruct/__init__.py
+++ openconcept/aerodynamics/openaerostruct/__init__.py
@@ -2,3 +2,4 @@ from .mesh_gen import TrapezoidalPlanformMesh, SectionPlanformMesh, ThicknessCho
 from .drag_polar import VLMDragPolar, VLMDataGen, VLM
 from .aerostructural import AerostructDragPolar, OASDataGen, Aerostruct, AerostructDragPolarExact
 from .CL_max_critical_section import CLmaxCriticalSectionVLM
+from .wave_drag import WaveDragFromSections
