f5d0a69379265d4ceee32622c2fbf05cdbd022a5
kanekosh <shugok@umich.edu>
2025-11-01 00:09:02 -0400

    readme -> README fixes build?

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ __version__ = re.findall(
 )[0]
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, "readme.md"), encoding="utf-8") as f:
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
